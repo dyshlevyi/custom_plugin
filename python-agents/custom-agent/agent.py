266adceb0d3fe4765c9b2fb4644ee3f1889005c1
admin <sergey.dyshlevyi@dataiku.com>
2025-12-30 17:04:59 +0000

    Edited file 'python-agents/custom-agent/agent.py'

--- python-agents/custom-agent/agent.py
+++ python-agents/custom-agent/agent.py
@@ -1,19 +1,3 @@
-import dataiku
-from dataiku.llm.python import BaseLLM
-
-# Add code here to define tools for instance
-
-class MyLLM(BaseLLM):
-    def __init__(self):
-        pass
-
-    def process(self, query, settings, trace):
-        prompt = query["messages"][-1]["content"]
-
-        # Add code here, for LLM completion, tool execution, etc...
-
-        return {"text": "the agent answer"}
-    
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents import create_openai_tools_agent
