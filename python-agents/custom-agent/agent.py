de16c3a5e58176ddd81fd45f4cf0241ca245f75b
admin <sergey.dyshlevyi@dataiku.com>
2025-12-30 17:04:38 +0000

    Edited file 'python-agents/custom-agent/agent.py'

--- python-agents/custom-agent/agent.py
+++ python-agents/custom-agent/agent.py
@@ -65,3 +65,39 @@ def generate_get_customer(dataset_name: str):
             return f"The customer's name is \"{name}\", holding the position \"{job}\" at the company named {company}"
         return f"No information can be found about the customer {customer_id}"
     return get_customer
+
+tools = [search_company_info]
+
+class MyLLM(BaseLLM):
+
+    def __init__(self):
+        pass
+
+    def set_config(self, config, plugin_config):
+        self.config = config
+        self.dataset = config.get("dataset_name")
+        tools.append(generate_get_customer(dataset_name=self.dataset))
+        self.agent = create_openai_tools_agent(model.with_config({"tags": ["agent_llm"]}), tools,
+                                               hub.pull("hwchase17/openai-tools-agent"))
+
+    async def aprocess_stream(self, query, settings, trace):
+        prompt = query["messages"][0]["content"]
+
+        tracer = LangchainToDKUTracer(dku_trace=trace)
+        agent_executor = AgentExecutor(agent=self.agent, tools=tools)
+
+        async for event in agent_executor.astream_events({"input": prompt}, version="v2",
+                                                         config={"callbacks": [tracer]}):
+            kind = event["event"]
+            if kind == "on_chat_model_stream":
+                content = event["data"]["chunk"].content
+                if content:
+                    yield {"chunk": {"text": content}}
+            elif kind == "on_tool_start":
+                # Event chunks are not part of the answer itself, but can provide progress information
+                yield {"chunk": {"type": "event", "eventKind": "tool_call", "eventData": {"name": event["name"]}}}
+
+                
+                
+                
+                
